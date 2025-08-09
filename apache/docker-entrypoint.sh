b93910b776f998364534395736d0d916dfd2c6e1
epo2000-sdh <sdh@epo2000.co.kr>
2025-08-09 23:14:14 +0900

    feat: add be & set nginx

--- apache/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -e
-
-/usr/local/bin/generate-certificate /usr/local/apache2
-/usr/local/bin/check-low-port
-
-/opt/modsecurity/activate-plugins.sh
-/opt/modsecurity/configure-rules.sh
-
-exec "$@"
